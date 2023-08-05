da9d64976b2a3ecd0d459cfcc6fa239b8c9e01cf
Ravi Bhankharia <ravibanks54@gmail.com>
2023-08-05 14:02:59 -0500

    Decouple PromptCMS client and message to execution cache from index.ts file.

---
+++ src/persistence/messageExecutionMapping.ts
@@ -0,0 +1,19 @@
+import {LRUCache} from "lru-cache";
+
+export default class MessageExecutionMapping {
+    private readonly messageToExecutionId: LRUCache<string, string>;
+    constructor() {
+        this.messageToExecutionId = new LRUCache<string, string>({
+            max: 500,
+        });
+    }
+
+    public get(messageId: string) {
+        return this.messageToExecutionId.get(messageId);
+    }
+
+    public set(messageId: string, executionId: string) {
+        return this.messageToExecutionId.set(messageId, executionId);
+    }
+
+}
